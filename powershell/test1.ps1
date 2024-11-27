61d9662e189b3fd4691bdafe33ba61888d55804e
W45 <73011833+wgs45@users.noreply.github.com>
2024-11-27 18:17:49 +0800

    Delete powershell directory

--- powershell/test1.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host 'Hello, world'
-# Get-Date # Display current date
-Get-History # print command history
-Get-Host # show host info
-Get-Timezone # Display current timezone
-
-
--- powershell/test2.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "All commands that act on Host:"
-Get-Command -Noun Host # Get all host commands
-Get-Date
