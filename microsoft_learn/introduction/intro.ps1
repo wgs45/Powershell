ef765af573e3ac7696c8bcc28543a6681e237b4c
W45 <73011833+wgs45@users.noreply.github.com>
2024-12-06 18:25:22 +0800

    Add files via upload

---
+++ microsoft_learn/introduction/intro.ps1
@@ -0,0 +1,15 @@
+# Get Powershell Version
+$PSVersionTable
+$PSVersionTable.PSVersion
+
+# Get verb commands
+Get-Verb
+
+# Get Powershell commands
+Get-Command
+Get-Command -Noun alias*
+Get-Command -Verb Get -Noun alias*
+
+# Get help
+Get-Help
+
