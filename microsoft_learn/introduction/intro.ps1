cdbaca2c4ebe364261ff0b3d582ddc0e02369a39
W45 <73011833+wgs45@users.noreply.github.com>
2024-12-07 12:57:23 +0800

    Add files via upload

--- microsoft_learn/introduction/intro.ps1
+++ microsoft_learn/introduction/intro.ps1
@@ -13,5 +13,16 @@ Get-Command -Noun File*
 Get-Command -Verb Get -Noun File*
 
 # Get help
-Get-Help
+Get-Help # List all available commands
+Get-Help Get-FileHash -Examples
+Get-Help -Name -Get-FileHash # Help with detailed explaination
+help Get-FileHash # More simple explaination
+help Get-FileHash -Examples
 
+# Discover objects by using Get-Member (Replace name-of-process with your actual process in your machine)
+Get-Process -Name 'name-of-process' | Get-Member
+Get-Process -Name 'name-of-process' | Get-Member | Select-Object Name, MemberType
+
+# Output running process on your machine
+Get-Process
+Get-Command -ParameterType Process
